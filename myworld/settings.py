ebf07edbceb04a9765156d35cf682476d196cb99
Monu Dahiya <monudahiya@Monus-MacBook-Air.local>
2025-03-30 07:10:50 +0530

    Blog post added

--- myworld/settings.py
+++ myworld/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'todo.apps.TodoConfig',
     'travello.apps.TravelloConfig',
     'django.contrib.admin',
--- myworld/urls.py
+++ myworld/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
     path('todo/', include('todo.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
